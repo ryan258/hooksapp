623bd3f22c4cc8e0785d3644b95d3ef8eab78444
Ryan Johnson <ryan@258webco.com>
2020-05-18 11:48:21 -0500

    Clean up boiler plate and set up SongList component.

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>Cheap Sunglasses</li>
+        <li>Handlebars</li>
+        <li>Stayin' Alive</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
